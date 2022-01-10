f018f96a6d823e2ce2c406524e0a3df53e33379d
kwangmon <kwangmony@gmail.com>
2022-01-10 23:48:24 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,11 +4,11 @@ export const search = (req, res) => res.send("Search"); // globalRouter
 ////////////////////////////////
 
 export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
+    return res.render("watch");
 }; //videoRouter
 
 export const edit = (req, res) => {
-    return res.send("Edit");
+    return res.render("edit");
 }; //videoRouter
 
 export const upload = (req, res) => res.send("Upload"); //videoRouter
