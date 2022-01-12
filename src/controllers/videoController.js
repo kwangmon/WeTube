92cbf1d38b9c4bf27eddff440dc6fe7c6b37de17
kwangmon <kwangmony@gmail.com>
2022-01-13 04:49:28 +0900

    fake db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,46 +1,70 @@
+const videos = [
+    {
+        title: "First Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:1,
+        id:1
+    },
+    {
+        title: "Second Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:2
+    },
+    {
+        title: "Third Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:3
+    },
+];
+
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating:5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views:59,
-            id:1
-        },
-        {
-            title: "Second Video",
-            rating:5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views:59,
-            id:1
-        },
-        {
-            title: "Third Video",
-            rating:5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views:59,
-            id:1
-        },
-    ];
-    return res.render("home", { PageTitle: "Home", videos })
+    return res.render("home", { pageTitle: "Home", videos })
 }; // globalRouter
-export const search = (req, res) => res.send("Search"); // globalRouter
 
 ////////////////////////////////
 
-export const see = (req, res) => {
-    return res.render("watch");
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 }; //videoRouter
 
-export const edit = (req, res) => {
-    return res.render("edit");
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 }; //videoRouter
 
-export const upload = (req, res) => res.send("Upload"); //videoRouter
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos [id -1].title = title;
+    return res.redirect(`/videos/${id}`);
+}; //videoRouter
 
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+}; //videoRouter
+  
+export const postUpload = (req, res) => {
+    const { title } = req.body;
+    const newVideo = {
+      title,
+      rating: 0,
+      comments: 0,
+      createdAt: "just now",
+      views: 0,
+      id: videos.length + 1,
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
 }; //videoRouter
